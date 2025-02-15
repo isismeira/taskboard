c4740cc7902a0b8b399a2ffb85723fbc4937544d
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-02-15 14:53:23 -0300

    Mudança no cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,11 @@ const cors = require('cors')
 const app = express();
 require('./config/dbConfig')
 
-app.use(cors())
+app.use(cors({
+    origin: "https://taskboard-flax.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 app.use(routes)
 
