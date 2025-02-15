166827804abf1576d8287d4c63afea83442e32ef
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-02-15 14:40:23 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000'
-})
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000"
+});
 
-export default api
+// Exemplo de requisição correta
+export const getAllNotes = async () => {
+    return await api.get("/api/annotations");
+};
+
+export default api;
