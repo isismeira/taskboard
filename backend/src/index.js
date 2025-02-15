c5cc4161037c5ef6f70610c6c65bbb3aed39d75b
Isis Meira <167538407+isismeira@users.noreply.github.com>
2025-02-15 15:13:49 -0300

    Revertendo mudanças

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,7 @@ const cors = require('cors')
 const app = express();
 require('./config/dbConfig')
 
-app.use(cors({
-    origin: "https://taskboard-flax.vercel.app",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-}));
+app.use(cors())
 app.use(express.json());
 app.use(routes)
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,7 @@
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000"
-});
+    baseURL: 'http://localhost:4000'
+})
 
-// Exemplo de requisição correta
-export const getAllNotes = async () => {
-    return await api.get("/api/annotations");
-};
-
-export default api;
+export default api
